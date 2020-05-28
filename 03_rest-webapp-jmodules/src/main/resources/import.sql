bde36ed636cafe3470eca5e9741aae02e1c80104
Patternbox <ehms@patternbox.com>
2020-05-28 11:44:44 +0200

    Initial REST app commit

---
+++ 03_rest-webapp-jmodules/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO person(id, name, address) VALUES(1, 'Hugo', 'Nürnberger Str. 15');
+INSERT INTO person(id, name, address) VALUES(2, 'Marlene', 'Am Schlachtensee 12');
+INSERT INTO person(id, name, address) VALUES(3, 'Kiki', 'Padderborner Str. 6');
+INSERT INTO person(id, name, address) VALUES(4, 'Hugo', 'Müllerstr. 123');
+INSERT INTO person(id, name, address) VALUES(5, 'Millers', 'Am Schlachtensee 48');
